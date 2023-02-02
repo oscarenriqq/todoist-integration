6b6c5e9dba26173940d8e84af978cf0fb3a76502
Oscar Molinar <molinar.o@latamdigitalmarketing.com>
2023-02-02 14:09:44 -0500

    Adding json encode response

--- app/Http/Controllers/TodoistController.php
+++ app/Http/Controllers/TodoistController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class TodoistController extends Controller
 {
     public function getAccess(Request $req) {
-        echo str([$req->input('code'), $req->input('state')]);
+        echo json_encode([$req->input('code'), $req->input('state')]);
     }
 }
